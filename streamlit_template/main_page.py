331e23439dd2155e6af9fb8e4a8efc9aa86e8320
matt-p-jones <102402679+matt-p-jones@users.noreply.github.com>
2024-02-14 18:29:54 -0700

    adding naviagation links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page")
 
-st.write("Click on a page to see racer stats")
+st.write("Click on a page to see racer or kart stats")
+link = '[To my Github Pages Site] (https://matt-p-jones.github.io/GitHubTest/)'
+st.markdown(link,unsafe_allow_html=True)
